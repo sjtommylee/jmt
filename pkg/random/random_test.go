eee8fb5deacd3eec1c839bbd7f7059bcdd414d49
sjtommylee <sjtommylee@gmail.com>
2024-02-04 00:04:04 -0800

    chore: commit save point

--- pkg/random/random_test.go
+++ pkg/random/random_test.go
@@ -21,3 +21,24 @@ func TestRandomInt(t *testing.T) {
 		t.Errorf("Result %d is not within the expected range [%d, %d]", result, low, high)
 	}
 }
+
+func TestRandomElem(t *testing.T) {
+	elements := []int{1, 2, 3, 4, 5}
+	randomElement := RandomElem(elements)
+	result := randomElement()
+
+	found := false
+
+	for _, v := range elements {
+		if v == result {
+
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Errorf("result %d is not one of the elements in the input slice of %v", result, elements)
+	}
+
+}
