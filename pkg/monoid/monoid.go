afed4f568ce1e2c72562bad8fbede451a2817da5
sjtommylee <sjtommylee@gmail.com>
2024-02-09 18:45:54 -0800

    chore: commit save point

--- pkg/monoid/monoid.go
+++ pkg/monoid/monoid.go
@@ -1,3 +1,15 @@
 package monoid
 
-type Monoid struct {}
+import "github.com/0xsj/fn-go/pkg/semigroup"
+
+type Monoid interface {
+	semigroup.Semigroup
+	Empty() interface{}
+}
+
+type StringMonoid struct{}
+
+// empty
+func (s StringMonoid) Empty() interface{} {
+	return ""
+}
