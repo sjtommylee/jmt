9dccb368ee7473d019125ace0ca5b6bcaab15fdb
sjtommylee <sjtommylee@gmail.com>
2024-02-18 14:04:58 -0800

    pipe, added tests

---
+++ examples/go.mod
@@ -0,0 +1,3 @@
+module github.com/0xsj/fn-go/examples
+
+go 1.21.6
---
+++ examples/main.go
@@ -0,0 +1,11 @@
+package examples
+
+// TODO
+func main() {
+	// result := p.Pipe(
+	// 	1,
+	// 	func(x int) int { return x + 1 },
+	// 	func(x int) int { return x * 2 },
+	// 	func(x int) int { return x - 1 },
+	// ).(int)
+}
