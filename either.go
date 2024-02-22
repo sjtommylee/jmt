06734bc793975eeca8e32fbc6a5379493a63154f
sjtommylee <sjtommylee@gmail.com>
2024-02-21 19:20:44 -0800

    WIP

--- either.go
+++ either.go
@@ -2,27 +2,32 @@ package fn
 
 // Represents the left variant of Either
 type Left[E any] struct {
-	Tag  string
-	Left interface{}
+	Tag   string
+	Value E
 }
 
 // Represents the right variant of Either
 type Right[A any] struct {
 	Tag   string
-	Right interface{}
+	Value A
 }
 
 // Either type with left, right variants
-type Either[E any, A any] interface{}
+type Either[E any, A any] interface {
+	IsLeft() bool
+	IsRight() bool
+	Left() E
+	Right() A
+}
 
 // Creates instance of Left
 func LeftConstructor[E any](value E) *Left[E] {
-	return &Left[E]{Tag: "Left", Left: value}
+	return &Left[E]{Tag: "Left", Value: value}
 }
 
 // Creates instance of Right
 func RightConstructor[A any](value A) *Right[A] {
-	return &Right[A]{Tag: "Right", Right: value}
+	return &Right[A]{Tag: "Right", Value: value}
 }
 
 // IsLeft returns true for Left instances, false otherwise
@@ -33,4 +38,14 @@ func (l *Left[E]) IsLeft() bool {
 // IsRight returns true for Right instances, false otherwise.
 func (r *Right[A]) IsRight() bool {
 	return false
+}
+
+// Left returns the value of the Left variant
+func (l *Left[E]) Left() E {
+	return l.Value
+}
+
+// Right returns the value of the Right variant
+func (r *Right[A]) Right() A {
+	return r.Value
 }
--- eq_test.go
+++ eq_test.go
@@ -1,1 +1,65 @@
 package fn
+
+import "testing"
+
+func TestLeftConstructor(t *testing.T) {
+	value := "error"
+	left := LeftConstructor(value)
+	if left.Tag != "Left" {
+		t.Errorf("Expected Tag to be 'Left', got %s", left.Tag)
+	}
+	if left.Value != value {
+		t.Errorf("Expected Value to be %s, got %s", value, left.Value)
+	}
+}
+
+func TestRightConstructor(t *testing.T) {
+	value := 42
+	right := RightConstructor(value)
+	if right.Tag != "Right" {
+		t.Errorf("Expected Tag to be 'Right', got %s", right.Tag)
+	}
+	if right.Value != value {
+		t.Errorf("Expected Value to be %d, got %d", value, right.Value)
+	}
+}
+
+func TestIsLeft(t *testing.T) {
+	left := LeftConstructor("error")
+	if !left.IsLeft() {
+		t.Error("Expected IsLeft to return true")
+	}
+
+	right := RightConstructor(42)
+	if right.IsLeft() {
+		t.Error("Expected IsLeft to return false")
+	}
+}
+
+func TestIsRight(t *testing.T) {
+	left := LeftConstructor("error")
+	if left.IsRight() {
+		t.Error("Expected IsRight to return false")
+	}
+
+	right := RightConstructor(42)
+	if !right.IsRight() {
+		t.Error("Expected IsRight to return true")
+	}
+}
+
+func TestLeft(t *testing.T) {
+	value := "error"
+	left := LeftConstructor(value)
+	if left.Left() != value {
+		t.Errorf("Expected Left() to return %s, got %s", value, left.Left())
+	}
+}
+
+func TestRight(t *testing.T) {
+	value := 42
+	right := RightConstructor(value)
+	if right.Right() != value {
+		t.Errorf("Expected Right() to return %d, got %d", value, right.Right())
+	}
+}
