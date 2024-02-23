fadcc9be91d22d7b3062a6fb7111e4f04d923137
sjtommylee <sjtommylee@gmail.com>
2024-02-22 19:32:32 -0800

    maybe, monad

--- maybe.go
+++ maybe.go
@@ -1,1 +1,25 @@
+// 
+
 package fn 
+
+// Maybe represents a value that may or may not be present
+
+type Maybe[T any] interface {
+	Map(func(T) T) Maybe[T]
+}
+
+// just - present value
+type Just[T any] struct {
+	Value T
+}
+
+func (j Just[T]) Map(fn func(T) T) Maybe[T] {
+	return Just[T]{Value: fn(j.Value)}
+}
+
+// nothing - represents an absent value 
+type Nothing[T any] struct {}
+
+func (n Nothing[T]) Map(fn func(T) T) Maybe[T] {
+	return n
+}
